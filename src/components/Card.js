54f1c41675852535d03a23eb4fe05cd5ce5f317a
Paul-adrien PETIT <petit.pauladrien@gmail.com>
2022-01-26 20:24:24 +0100

    Ajout fonctionalité coup de coeur avec axios + filtre top flop

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Card = ({ movie }) => {
   const dateFormater = (date) => {
@@ -75,6 +74,17 @@ const Card = ({ movie }) => {
     return genreArray.map((genre) => <li key={genre}>{genre}</li>);
   };
 
+  const addStorage = () => {
+    let storedData = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    if (!storedData.includes(movie.id.toString())) {
+      storedData.push(movie.id);
+      window.localStorage.movies = storedData;
+    }
+  };
+
   return (
     <div className="card">
       <img
@@ -94,11 +104,17 @@ const Card = ({ movie }) => {
       <h4>
         {movie.vote_average}/10 <span>⭐</span>
       </h4>
-      <ul>{genreFinder()}</ul>
+      <ul>
+        {movie.genre_ids
+          ? genreFinder()
+          : movie.genres.map((genre) => <li>{genre.name}</li>)}
+      </ul>
       {movie.overview ? <h3>Synopsis</h3> : ""}
       <p>{movie.overview}</p>
 
-      <div className="btn">Ajouter aux coups de coeur</div>
+      <div className="btn1" onClick={() => addStorage()}>
+        Ajouter aux coups de coeur
+      </div>
     </div>
   );
 };
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import Card from "../components/Card";
 
 const UserList = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=660dc479679e452766ff863e22c07312&language=fr-FR`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coup de coeur <span>💝</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((movie) => <Card movie={movie} key={movie.id} />)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
